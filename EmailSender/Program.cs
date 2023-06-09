43f1a6547dcecffe081efe6446c2e5029da330e5
A-Ashraf-Dev <a.ashraf@sanasofteg.com>
2023-06-10 00:11:21 +0300

    Add project files.

---
+++ EmailSender/Program.cs
@@ -0,0 +1,43 @@
+using EmailSender.ApplicationDBContext;
+using Microsoft.EntityFrameworkCore;
+using NToastNotify;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+// Please check the connection details in the appsettings.json the DB name will be EmailSender
+var dbConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlServer(dbConnection));
+
+builder.Services.AddMvc().AddNToastNotifyToastr(new ToastrOptions()
+{
+    ProgressBar = true,
+    PositionClass = ToastPositions.TopRight,
+    PreventDuplicates = true,
+    CloseButton = true,
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Messages}/{action=CreateAndShowMessages}/{id?}");
+
+app.Run();
